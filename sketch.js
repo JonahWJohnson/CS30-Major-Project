fe20a4d6716d740efc1ecbe83af82146ef2e0506
JonahWJohnson <146984197+JonahWJohnson@users.noreply.github.com>
2023-11-16 11:40:59 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,6 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let mouseX;
-let mouseY;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
