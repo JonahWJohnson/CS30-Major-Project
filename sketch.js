4f3a2172a1a2c19fe9e92b82f463e6ec52e9becd
JonahWJohnson <146984197+JonahWJohnson@users.noreply.github.com>
2023-12-04 11:47:13 -0600

    worked on major project

--- sketch.js
+++ sketch.js
@@ -4,14 +4,26 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let player = {
+  x : width/2,
+  y : height/2,
+  dx : 5,
+  dy : 0,
+  weight,
+  width : 10,
+  height : 10,
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  world.gravity.y = 10
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 20)
+  updatePlayer();
+  displayPlayer();
 }
 
 class Enemies {
@@ -19,4 +31,13 @@ class Enemies {
     this.x = x;
     this.y = y
   }
+}
+
+function displayPlayer() {
+  square(player.x, player.y, 10);
+}
+
+function updatePlayer() {
+  player.x += player.dx;
+  player.y += player.dy;
 }
