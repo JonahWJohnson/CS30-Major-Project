595306e62b7312c2cb59e876f762fdff80a23821
JonahWJohnson <146984197+JonahWJohnson@users.noreply.github.com>
2023-12-12 12:11:53 -0600

    tweaked movement

--- sketch.js
+++ sketch.js
@@ -2,14 +2,15 @@ let player;
 
 function setup() {
 	new Canvas(1600, 900);
-	world.gravity.y = 10;
+	world.gravity.y = 20;
 
 	player = new Sprite();
 	player.y = height/2;
 	player.x = width/2;
-	player.dx = 5;
+	player.dx = 0;
 	player.radius = 50;
 	player.friction = 2
+	player.dashLength = 20
 	player.grounded = false
 	player.canDash = true
 
@@ -30,23 +31,27 @@ function draw() {
 function playerMovement() {
 	// left right movement
 	if (kb.pressing('d')) {
-		player.vel.x = 10;
+		player.vel.x = 10 + player.dx;
 
 	}
 	else if (kb.pressing('a')) {
-		player.vel.x = -10;
+		player.vel.x = -10 + player.dx;
 	}
 	else {
-		player.vel.x = 0;
+		player.vel.x = player.dx;
 	}
 
-	if (kb.presses('space') && player.grounded) {
-		player.vel.y = 10;
+	if (kb.presses('space')) {
+		player.vel.y = -10;
 	}
 
 // shift/dash
-	if (kb.presses) {
-		
+	if (kb.presses('shift')) {
+		if (kb.pressing('d')) {
+			for (i = 0; i < player.dashLength; i++) {
+				player.dx += 1
+			}
+		}	
 	}
 }
 
@@ -57,11 +62,5 @@ function dash() {
 }
 
 function checkColide() {
-	if (player.collide(floor)) {
-		player.grounded = true;
-	}
-	else {
-		player.grounded = false
-	}
 
 }
